e04c800725a1f4bf459f45c1eb3fc066b7830d33
Hridoy <rakibhasanhridoy776@gmail.com>
2022-05-24 17:13:13 +0600

    tool with specific id api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -21,6 +21,13 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
        app.get('/tools',async(req,res)=>{
            const query={}
            const result= await toolCollection.find(query).toArray()
+           res.send(result)
+       })
+
+       app.get('/tool/:id', async(req,res)=>{
+           const id=req.params.id;
+           const query={_id:ObjectId(id)}
+           const result =await toolCollection.findOne(query)
            res.send(result)
        })
 
