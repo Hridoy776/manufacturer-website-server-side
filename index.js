bc53e027716b70941f9d74c65a223b3e83f2a647
Hridoy <rakibhasanhridoy776@gmail.com>
2022-05-24 18:01:35 +0600

    user update

--- index.js
+++ index.js
@@ -12,37 +12,52 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g9ujg.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-  
-  async function run(){
-      try {
-       await client.connect()
-       const toolCollection = client.db('DrillDestructor').collection('tools'); 
 
-       app.get('/tools',async(req,res)=>{
-           const query={}
-           const result= await toolCollection.find(query).toArray()
-           res.send(result)
-       })
+async function run() {
+    try {
+        await client.connect()
+        const toolCollection = client.db('DrillDestructor').collection('tools');
+        const userCollection = client.db('DrillDestructor').collection('users');
 
-       app.get('/tool/:id', async(req,res)=>{
-           const id=req.params.id;
-           const query={_id:ObjectId(id)}
-           const result =await toolCollection.findOne(query)
-           res.send(result)
-       })
+        app.get('/tools', async (req, res) => {
+            const query = {}
+            const result = await toolCollection.find(query).toArray()
+            res.send(result)
+        })
 
-      } finally{
-          
-      }
-  }
-  run().catch(console.dir)
+        app.get('/tool/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await toolCollection.findOne(query)
+            res.send(result)
+        })
+
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email }
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user
+            }
+            const result = await userCollection.updateOne(filter, updateDoc, options)
+            res.send(result)
+
+
+        })
+
+    } finally {
+
+    }
+}
+run().catch(console.dir)
 
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
